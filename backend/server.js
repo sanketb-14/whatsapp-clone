225821e4be6b8fb849811848522dcc94e1a29346
unknown <sanketbarapatre999@gmail.com>
2025-08-12 01:49:56 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:5173" }));
+app.use(cors({ origin: process.env.FRONTEND_URL || "*" }));
 
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI;
